aa4731ec44c4adac3bdc2fdcf27cff0a1978ce9f
Al Williams <ltc_ahw@yahoo.com>
2019-10-18 01:19:47 -0400

    all script consolidated into script file

--- p1/script/main.js
+++ p1/script/main.js
@@ -3,4 +3,12 @@ var app = new Vue({
     data: {
       dog: 'Heidi'
     }
-  })
+  });
+  
+  function changeImage(pic,txt) {
+    document.getElementById("main_img").src = pic;
+    document.getElementById("out_put_txt").innerHTML = txt;
+}
+function returnImage() {
+  changeImage("img/IMG_0062.jpg","");
+}
