fa2d25f38dd3827760a62e744f57df527451f1ee
Al Williams <ltc_ahw@yahoo.com>
2019-11-07 16:29:28 -0500

    P2 upload (Baseline)
    P2 catch Up

---
+++ p2/script/main.js
@@ -0,0 +1,21 @@
+var app = new Vue({
+    el: '#app',
+    data:{
+      dog: 'Heidi',
+      out_put_text:' Please: Sit Pretty'
+    },
+    methods: {
+       changeImage: function (pic,txt) {
+    document.getElementById("main_img").src = this.pic;
+    this.out_put_text = txt;
+       return this.out_put_text ;    
+        }
+ //      returnImage: function () {
+ //               changeImage("img/IMG_0062.jpg","");
+       }
+  });
+
+
+//function returnImage() {
+//  changeImage("img/IMG_0062.jpg","");
+//}
