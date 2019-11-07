b9d8320b2f7e00953a2f74f8331d41024d4bba60
Al Williams <ltc_ahw@yahoo.com>
2019-11-07 16:14:43 -0500

    Follow Up on P1
    Follow Up on P1

--- p1/script/main.js
+++ p1/script/main.js
@@ -1,14 +1,21 @@
 var app = new Vue({
     el: '#app',
-    data: {
-      dog: 'Heidi'
-    }
+    data:{
+      dog: 'Heidi',
+      out_put_text:' Please: Sit Pretty'
+    },
+    methods: {
+       changeImage: function (pic,txt) {
+    document.getElementById("main_img").src = this.pic;
+    this.out_put_text = txt;
+       return this.out_put_text ;    
+        }
+ //      returnImage: function () {
+ //               changeImage("img/IMG_0062.jpg","");
+       }
   });
-  
-  function changeImage(pic,txt) {
-    document.getElementById("main_img").src = pic;
-    document.getElementById("out_put_txt").innerHTML = txt;
-}
-function returnImage() {
-  changeImage("img/IMG_0062.jpg","");
-}
+
+
+//function returnImage() {
+//  changeImage("img/IMG_0062.jpg","");
+//}
