7d4881a81108a20f3eafcdb747bf5517dd4fb9b8
Al Williams <ltc_ahw@yahoo.com>
2019-11-07 20:45:35 -0500

    part 2

---
+++ p2/HeidiTricks.vue
@@ -0,0 +1,8 @@
+<template>
+<div v-bind:class="{ result : result == 'Trick'}"
+    <ul>
+    <li>Attempt #: {{ try }}</li> 
+    <li>Attempt #: {{ trick }}</li> 
+    </ul>
+    <button @click="$emit('delete-round',)> Bark </button> 
+</template> 
